12a7cbe035be8a5fca492d90a85b5742717537ec
Aaron Moseley <amoseley018@gmail.com>
2026-02-24 22:24:11 -0500

    Added performance logging with WandB

--- Training/BasicDepthModelTrainer.py
+++ Training/BasicDepthModelTrainer.py
@@ -1,9 +1,10 @@
 import torch
 
-from Training.Trainer import Trainer, TrainerInitializationData, DEFAULT_BATCH_SIZE, DEFAULT_EPOCHS, DEFAULT_LEARN_RATE
+from Training.Trainer import Trainer, TrainerInitializationData, CallbackIntervalType, DEFAULT_BATCH_SIZE, DEFAULT_EPOCHS, DEFAULT_LEARN_RATE
 from Datasets.DepthPredictionDataset import DepthPredictionDataset
 from Models.BasicDepthPredictionModel import DepthPredictionModel
 from Utilities.LossFunctions import ScaleInvariantLoss
+from Utilities.WandBPerformanceTracker import WeightsAndBiasesTracker
 
 DATASET_SCALE_FACTORS = {
         "KITTI": 256.0,
@@ -12,7 +13,7 @@ DATASET_SCALE_FACTORS = {
     }
 
 class BasicDepthModelTrainer(Trainer):
-    def __init__(self, datasetName:str, device:torch.device, validationSetRatio:float=0.1, batchSize:int=DEFAULT_BATCH_SIZE, epochs:int=DEFAULT_EPOCHS, learnRate:float=DEFAULT_LEARN_RATE) -> None:
+    def __init__(self, datasetName:str, device:torch.device, runName:str, validationSetRatio:float=0.1, batchSize:int=DEFAULT_BATCH_SIZE, epochs:int=DEFAULT_EPOCHS, learnRate:float=DEFAULT_LEARN_RATE) -> None:
         initializationData = TrainerInitializationData()
 
         scaleFactor = DATASET_SCALE_FACTORS[datasetName]
@@ -40,14 +41,34 @@ class BasicDepthModelTrainer(Trainer):
         initializationData.optimizer = torch.optim.RAdam(model.parameters(), learnRate)
         initializationData.lrScheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(initializationData.optimizer, 10)
 
+        self.lossLogInterval = 10
+        self.imageLogInterval = 100
+
         super().__init__(initializationData)
+
+        self.logger = WeightsAndBiasesTracker("Depth Prediction", runName, {
+            "batchSize": self.batchSize,
+            "learnRate": self.learnRate,
+            "epochs": self.epochs,
+            "dataset": datasetName
+        })
+
+        self.AddCallback(self.LogEpochLoss, intervalType=CallbackIntervalType.EVERY_N_EPOCHS, interval=1)
 
     def TrainingStep(self, inputData:tuple) -> torch.Tensor:
         inputTensor, gtTensor, validMask = inputData
 
         outputTensor = self.model(inputTensor)
 
-        loss = self.lossFunction(outputTensor, gtTensor, validMask)
+        loss:torch.Tensor = self.lossFunction(outputTensor, gtTensor, validMask)
+
+        if self.currentTrainingBatchIndex % self.lossLogInterval == 0:
+            self.logger.LogData({
+                "trainingLoss": loss.detach().item()
+            }, step=self.currentTrainingBatchIndex)
+
+        if self.currentTrainingBatchIndex % self.imageLogInterval == 0:
+            self.logger.LogImage([inputTensor, gtTensor, validMask, outputTensor], self.currentEpoch, self.currentTrainingBatchIndex)
 
         return loss
 
@@ -59,3 +80,9 @@ class BasicDepthModelTrainer(Trainer):
         loss = self.lossFunction(outputTensor, gtTensor, validMask)
 
         return loss
+    
+    def LogEpochLoss(self) -> None:
+        self.logger.LogData({
+            "averageTrainingLoss": self.currentAverageTrainingLoss.detach().item(),
+            "averageValidationLoss": self.currentAverageValidationLoss.detach().item()
+        }, step=self.currentEpoch)
---
+++ Utilities/WandBPerformanceTracker.py
@@ -0,0 +1,77 @@
+import wandb
+import torch
+import numpy as np
+
+class WeightsAndBiasesTracker():
+    def __init__(self, projectName:str, runName:str, configData:dict) -> None:
+        self.projectName = projectName
+        self.runName = runName
+        
+        runConfig = configData.copy()
+
+        if len(runName) > 0:
+            runConfig["runName"] = runName
+        
+        self.run = wandb.init(
+            entity="amoseley018",
+            project=projectName,
+            config=runConfig
+        )
+
+    def LogData(self, data:dict, step:int=None) -> None:
+        if step is None:
+            self.run.log(data)
+        else:
+            self.run.log(data, step=step)
+
+    def LogImage(self, tensorList:list[torch.Tensor], epochIndex:int, batchIndex:int) -> None:
+        widthSum = 0
+        maxHeight = 0
+        batchSize = 0
+        
+        #convert image tensor to numpy array
+        arrayList:list[np.ndarray] = []
+        for tensor in tensorList:
+            newArray = tensor.detach().cpu().numpy()
+
+            newArray /= max(np.max(newArray), 1.0)
+            newArray *= 255.0
+            newArray = np.asarray(newArray, dtype=np.uint8)
+
+            currentBatchSize, channels, currentHeight, currentWidth = newArray.shape
+
+            widthSum += currentWidth
+            maxHeight = max(maxHeight, currentHeight)
+            batchSize = max(batchSize, currentBatchSize)
+
+            if channels == 1:
+                newArray = np.tile(newArray, (1, 3, 1, 1))
+
+            arrayList.append(newArray)
+
+        sideBySideArray = np.zeros((batchSize, 3, maxHeight, widthSum), dtype=np.uint8)
+
+        startingPosition = 0
+        for imageArray in arrayList:
+            endPosition = startingPosition + imageArray.shape[3]
+            arrayHeight = imageArray.shape[2]
+
+            sideBySideArray[:, :, :arrayHeight, startingPosition:endPosition] = imageArray
+
+            startingPosition = endPosition
+
+        #create new empty array, stack the arrays vertically
+        stackedArray = np.zeros((3, maxHeight * batchSize, widthSum), dtype=np.uint8)
+        for i in range(batchSize):
+            stackedArray[:, i * maxHeight:(i + 1) * maxHeight, :] = sideBySideArray[i, :, :, :]
+
+        stackedArray = np.transpose(stackedArray, axes=(1, 2, 0))
+
+        #log the image
+        finalImage = wandb.Image(stackedArray, caption=f"Epoch: {epochIndex}, Batch: {batchIndex}")
+        self.run.log({
+            "images": [finalImage]
+        })
+
+    def FinishRun(self) -> None:
+        self.run.finish()
